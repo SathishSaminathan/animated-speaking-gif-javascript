d6a04d06326d3a34be703813df462f82d0bd66ae
SathishSaminathan <Sathishcse1975@gmail.com>
2020-04-27 13:22:34 +0530

    commit for ip changes

--- fb.js
+++ fb.js
@@ -1,7 +1,19 @@
+function getParameterByName(name, url) {
+  if (!url) url = window.location.href;
+  url = window.location.href;
+  name = name.replace(/[\[\]]/g, "\\$&");
+  var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
+    results = regex.exec(url);
+  if (!results) return null;
+  if (!results[2]) return "";
+  let ip = decodeURIComponent(results[2].replace(/\+/g, " "));
+  return ip.replace(/[.]/g, "");
+}
 
 const dbRef = firebase.database().ref();
-const usersRef = dbRef.child("bot");
-
+// const usersRef = dbRef.child("bot");
+const usersRef = dbRef.child(getParameterByName("key"));
+// console.log("print ip", getParameterByName("key"));
 usersRef.on("child_changed", (snap) => {
   let user = snap.val();
   console.log(user);
